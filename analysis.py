3e00abdc36f5df0702b8c4e586c60eee4a5d09b7
Golfing7 <atroo@me.com>
2025-11-03 16:53:57 -0500

    Add extra comparison levels

--- analysis.py
+++ analysis.py
@@ -23,6 +23,9 @@ def welch_ttest(x, y):
 
 def main():
     comparison_pairs = [
+        ("VANILLA", "EIGENCRAFT", 4),
+        ("VANILLA", "EIGENCRAFT", 6),
+        ("VANILLA", "EIGENCRAFT", 8),
         ("VANILLA", "EIGENCRAFT", 10),
         ("VANILLA", "ALTERNATE_CURRENT", 20),
         ("EIGENCRAFT", "ALTERNATE_CURRENT", 1),
@@ -37,10 +40,10 @@ def main():
         data1_off = df1.PowerOffNanos
         data2_off = df2.PowerOffNanos * pair[2]
 
-        print(f"Comparing {pair[0]} and {pair[1]} POWER ON")
+        print(f"Comparing {pair[0]} and {pair[1]} POWER ON Factor {pair[2]}")
         welch_ttest(data1_on, data2_on)
 
-        print(f"Comparing {pair[0]} and {pair[1]} POWER OFF")
+        print(f"Comparing {pair[0]} and {pair[1]} POWER OFF Factor {pair[2]}")
         welch_ttest(data1_off, data2_off)
 
 
