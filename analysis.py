2193006584121881c574564c3b8e63ce4ddba95c
Golfing7 <atroo@me.com>
2025-11-13 18:20:18 -0500

    Change analysis to full flood

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import scipy.stats as stats
 import pandas as pd
 
@@ -10,7 +11,7 @@ def wilcoxon_test(x, y):
           f"p-value = {p}", "\n")
     return 1
 
-def analyze_algorithm(experiment):
+def analyze_algorithm(experiments):
     comparison_pairs = [
         ("VANILLA", "EIGENCRAFT", 4),
         ("VANILLA", "EIGENCRAFT", 6),
@@ -21,13 +22,17 @@ def analyze_algorithm(experiment):
     ]
 
     for pair in comparison_pairs:
-        df1 = pd.read_csv(f"{data_directory}/{experiment}/{pair[0]}/data.csv")
-        df2 = pd.read_csv(f"{data_directory}/{experiment}/{pair[1]}/data.csv")
-
-        data1_on = df1.PowerOnNanos
-        data2_on = df2.PowerOnNanos * pair[2]
-        data1_off = df1.PowerOffNanos
-        data2_off = df2.PowerOffNanos * pair[2]
+        data1_on = []
+        data2_on = []
+        data1_off = []
+        data2_off = []
+        for experiment in experiments:
+            df1 = pd.read_csv(f"{data_directory}/{experiment}/{pair[0]}/data.csv")
+            df2 = pd.read_csv(f"{data_directory}/{experiment}/{pair[1]}/data.csv")
+            data1_on.extend(df1.PowerOnNanos.values)
+            data2_on.extend(df2.PowerOnNanos.values * pair[2])
+            data1_off.extend(df1.PowerOffNanos.values)
+            data2_off.extend(df2.PowerOffNanos.values * pair[2])
 
         print(f"Comparing {pair[0]} and {pair[1]} POWER ON Factor {pair[2]}")
         wilcoxon_test(data1_on, data2_on)
@@ -36,7 +41,7 @@ def analyze_algorithm(experiment):
         wilcoxon_test(data1_off, data2_off)
 
 def main():
-    analyze_algorithm(experiment_use)
+    analyze_algorithm([f"{experiment_use}_{i}" for i in range(1, 15)])
 
 
 if __name__ == "__main__":
