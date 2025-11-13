8a6e230e5d17967bdcc961c49040d493b98c0b25
Golfing7 <atroo@me.com>
2025-11-13 15:48:55 -0500

    Update test used

--- analysis.py
+++ analysis.py
@@ -1,27 +1,16 @@
 import scipy.stats as stats
 import pandas as pd
 
-experiment = "flood"
+experiment_use = "flood"
 data_directory = "./data"
 
+def wilcoxon_test(x, y):
+    t, p = stats.wilcoxon(x, y, alternative="greater")
+    print(f"Wilcoxon test= {t:4f}", "\n",
+          f"p-value = {p}", "\n")
+    return 1
 
-def welch_ttest(x, y):
-    """
-    Taken from https://pythonfordatascienceorg.wordpress.com/welch-t-test-python-pandas/
-    :param x: the first sample
-    :param y: the second sample
-    """
-    ## Welch-Satterthwaite Degrees of Freedom ##
-    dof = (x.var() / x.size + y.var() / y.size) ** 2 / (
-                (x.var() / x.size) ** 2 / (x.size - 1) + (y.var() / y.size) ** 2 / (y.size - 1))
-
-    t, p = stats.ttest_ind(x, y, equal_var=False, alternative='greater')
-
-    print(f"Welch's t-test= {t:.4f}", "\n",
-          f"p-value = {p}", "\n",
-          f"Welch-Satterthwaite Degrees of Freedom= {dof:.4f}", "\n")
-
-def main():
+def analyze_algorithm(experiment):
     comparison_pairs = [
         ("VANILLA", "EIGENCRAFT", 4),
         ("VANILLA", "EIGENCRAFT", 6),
@@ -41,10 +30,13 @@ def main():
         data2_off = df2.PowerOffNanos * pair[2]
 
         print(f"Comparing {pair[0]} and {pair[1]} POWER ON Factor {pair[2]}")
-        welch_ttest(data1_on, data2_on)
+        wilcoxon_test(data1_on, data2_on)
 
         print(f"Comparing {pair[0]} and {pair[1]} POWER OFF Factor {pair[2]}")
-        welch_ttest(data1_off, data2_off)
+        wilcoxon_test(data1_off, data2_off)
+
+def main():
+    analyze_algorithm(experiment_use)
 
 
 if __name__ == "__main__":
